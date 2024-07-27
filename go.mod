199d477ce57998c527eb1a395499ba509db85716
jgill <jgill@flourishsoftware.com>
2024-07-27 04:31:08 -0400

    add hello

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/atljoseph/gomaid-dep-5
 
 go 1.22.1
+
+require github.com/atljoseph/gomaid-dep-6 v0.0.0-20240727082945-25ac128c0d32
---
+++ hello/hello.go
@@ -0,0 +1,9 @@
+package hello
+
+import (
+	"github.com/atljoseph/gomaid-dep-6/utils"
+)
+
+func SayHello() {
+	utils.SayHelloFrom("gomaid-dep-5")
+}
